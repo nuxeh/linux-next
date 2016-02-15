vartom/linux

clk: tegra: Implement SOR1 clock on Tegra210
SOR1 clock uses a two-stage mux to select a parent. This is difficult to
represent in software, especially if drivers need to select one of the
parents during the programming of the SOR module.

To model the two-stage mux using a single level of parents, use a parent
and mask/value mapping.

Signed-off-by: Thierry Reding <treding@nvidia.com>

drivers/clk/tegra/clk-tegra210.c
@@ -2318,6 +2318,20 @@ static void tegra210_utmi_param_configure(void __iomem *clk_base)
 	writel_relaxed(reg, clk_base + UTMIPLL_HW_PWRDN_CFG0);
 }
 
+static const char *tegra_clk_sor1_parents[] = {
+	"sor_safe", "sor1_brick", "pll_p_out0", "pll_d_out0", "pll_d2_out0",
+	"clk_m"
+};
+
+static const struct tegra_clk_parent tegra_clk_sor1_mux[] = {
+	{ "sor_safe",    0x0000c000, 0x00000000 },
+	{ "sor1_brick",  0x0000c000, 0x00004000 },
+	{ "pll_p_out0",  0xe000c000, 0x00008000 },
+	{ "pll_d_out0",  0xe000c000, 0x40008000 },
+	{ "pll_d2_out0", 0xe000c000, 0xa0008000 },
+	{ "clk_m",       0xe000c000, 0xc0008000 },
+};
+
 static __init void tegra210_periph_clk_init(void __iomem *clk_base,
 					    void __iomem *pmc_base)
 {
@@ -2339,6 +2353,12 @@ static __init void tegra210_periph_clk_init(void __iomem *clk_base,
 	clk = tegra_clk_register_periph_fixed("sor_safe", "pll_p", 0, clk_base,
 					      1, 17, 222);
 	clks[TEGRA210_CLK_SOR_SAFE] = clk;
+
+	clk = tegra_clk_register_sor("sor1", clk_base, tegra_clk_sor1_parents,
+				     tegra_clk_sor1_mux,
+				     ARRAY_SIZE(tegra_clk_sor1_parents), 0,
+				     0x410, 183);
+	clks[TEGRA210_CLK_SOR1] = clk;
 
 	/* plld_dsi */
 	clk = clk_register_gate(NULL, "pll_d_dsi_out", "pll_d_out0", 0,

nuxeh/linux-next

drivers/clk/tegra/clk-tegra210.c
@@ -2452,6 +2452,20 @@ static void tegra210_utmi_param_configure(void __iomem *clk_base)
 	writel_relaxed(reg, clk_base + UTMIPLL_HW_PWRDN_CFG0);
 }
 
+static const char *tegra_clk_sor1_parents[] = {
+	"sor_safe", "sor1_brick", "pll_p_out0", "pll_d_out0", "pll_d2_out0",
+	"clk_m"
+};
+
+static const struct tegra_clk_parent tegra_clk_sor1_mux[] = {
+	{ "sor_safe",    0x0000c000, 0x00000000 },
+	{ "sor1_brick",  0x0000c000, 0xa0004000 },
+	{ "pll_p_out0",  0xe000c000, 0x00008000 },
+	{ "pll_d_out0",  0xe000c000, 0x40008000 },
+	{ "pll_d2_out0", 0xe000c000, 0xa0008000 },
+	{ "clk_m",       0xe000c000, 0xc0008000 },
+};
+
 static __init void tegra210_periph_clk_init(void __iomem *clk_base,
 					    void __iomem *pmc_base)
 {
@@ -2473,6 +2487,12 @@ static __init void tegra210_periph_clk_init(void __iomem *clk_base,
 	clk = tegra_clk_register_periph_fixed("sor_safe", "pll_p", 0, clk_base,
 					      1, 17, 222);
 	clks[TEGRA210_CLK_SOR_SAFE] = clk;
+
+	clk = tegra_clk_register_sor("sor1", clk_base, tegra_clk_sor1_parents,
+				     tegra_clk_sor1_mux,
+				     ARRAY_SIZE(tegra_clk_sor1_parents), 0,
+				     0x410, 183);
+	clks[TEGRA210_CLK_SOR1] = clk;
 
 	/* pll_d_dsi_out */
 	clk = clk_register_gate(NULL, "pll_d_dsi_out", "pll_d_out0", 0,