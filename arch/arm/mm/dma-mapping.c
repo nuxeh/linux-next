34a69829ff8f7bc3c59de10e41c63fd6050e15b9
Alexandre Courbot <acourbot@nvidia.com>
2016-02-26 17:36:02 +0900

    ARM: dma-mapping: remove arm_dma_set_mask()
    arm_dma_set_mask() implements exactly the same behavior as the fallback
    that dma_set_mask() takes if the set_dma_mask op is not set. Remove it
    and use that fallback instead like the dma_get_mask() counterpart
    already does.
    
    Signed-off-by: Alexandre Courbot <acourbot@nvidia.com>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -141,7 +141,6 @@ struct dma_map_ops arm_dma_ops = {
 	.sync_single_for_device	= arm_dma_sync_single_for_device,
 	.sync_sg_for_cpu	= arm_dma_sync_sg_for_cpu,
 	.sync_sg_for_device	= arm_dma_sync_sg_for_device,
-	.set_dma_mask		= arm_dma_set_mask,
 };
 EXPORT_SYMBOL(arm_dma_ops);
 
@@ -160,7 +159,6 @@ struct dma_map_ops arm_coherent_dma_ops = {
 	.get_sgtable		= arm_dma_get_sgtable,
 	.map_page		= arm_coherent_dma_map_page,
 	.map_sg			= arm_dma_map_sg,
-	.set_dma_mask		= arm_dma_set_mask,
 };
 EXPORT_SYMBOL(arm_coherent_dma_ops);
 
@@ -1003,16 +1001,6 @@ int dma_supported(struct device *dev, u64 mask)
 	return __dma_supported(dev, mask, false);
 }
 EXPORT_SYMBOL(dma_supported);
-
-int arm_dma_set_mask(struct device *dev, u64 dma_mask)
-{
-	if (!dev->dma_mask || !dma_supported(dev, dma_mask))
-		return -EIO;
-
-	*dev->dma_mask = dma_mask;
-
-	return 0;
-}
 
 #define PREALLOC_DMA_DEBUG_ENTRIES	4096
 
@@ -1867,8 +1855,6 @@ struct dma_map_ops iommu_ops = {
 	.unmap_sg		= arm_iommu_unmap_sg,
 	.sync_sg_for_cpu	= arm_iommu_sync_sg_for_cpu,
 	.sync_sg_for_device	= arm_iommu_sync_sg_for_device,
-
-	.set_dma_mask		= arm_dma_set_mask,
 };
 
 struct dma_map_ops iommu_coherent_ops = {
@@ -1882,8 +1868,6 @@ struct dma_map_ops iommu_coherent_ops = {
 
 	.map_sg		= arm_coherent_iommu_map_sg,
 	.unmap_sg	= arm_coherent_iommu_unmap_sg,
-
-	.set_dma_mask	= arm_dma_set_mask,
 };
 
 /**
